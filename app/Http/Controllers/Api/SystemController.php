c365d8d98fe2fb3d38b47b013860d533054d058d
yangtingjin <ytj@sumsia.com>
2018-12-16 14:45:23 +0800

    22

--- app/Http/Controllers/Api/SystemController.php
+++ app/Http/Controllers/Api/SystemController.php
@@ -19,6 +19,33 @@ class SystemController extends Controller
 
     }
 
+    public function systemUpdate(Request $request)
+    {
+        $data = Setting::first();
+        $data->id = $request->input('id');
+        $data->cashout_max = $request->input('cashout_max');
+        $data->cashout_min = $request->input('cashout_min');
+        $data->cashout_rate = $request->input('cashout_rate');
+        $data->register_award = $request->input('register_award');
+        $data->first_publish_award = $request->input('first_publish_award');
+        $data->finish_1_return = $request->input('finish_1_return');
+        $data->finish_2_return = $request->input('finish_2_return');
+        $data->finish_3_return = $request->input('finish_3_return');
+        $data->register_1_return = $request->input('register_1_return');
+        $data->register_2_return = $request->input('register_2_return');
+        $data->register_3_return = $request->input('register_3_return');
+        $data->created_at = $request->input('created_at');
+        $data->updated_at = $request->input('updated_at');
+        $data->withdraw_status = $request->input('withdraw_status');
+        $data->model_text = $request->input('model_text');
+        $data->is_model_close = $request->input('is_model_close');
+        $data->limit_1_return = $request->input('limit_1_return');
+        $data->limit_2_return = $request->input('limit_2_return');
+        $data->limit_3_return = $request->input('limit_3_return');
+        $data->save();
+        return $this->responseOK('修改成功',[]);
+    }
+
     public function user(Request $request)
     {
         $data = UserSetting::get();
